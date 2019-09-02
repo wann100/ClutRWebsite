e04a818efb8617f3a7086902d30fc61afa70f2a4
Mamadou Wann <mamadouwann@gmail.com>
2019-09-01 18:34:52 -0600

    Create ServiceAction
    Creating backend in order to edit services! Need to make sure this data is admin locked

---
+++ actions/ServicesAction.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import { returnStatement } from "@babel/types";
+
+
+export function getServices(){
+
+ var returnStatement = 0;
+//Where I will get current services so I can display them
+
+return returnStatement;
+
+}
+
+export function updateService(whichservice,details){
+
+    
+}
